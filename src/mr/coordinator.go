dc63424d4e17f0f8940b116e6150636666e2f518
Akshob Gopalakrishna <akgop@microsoft.com>
2024-05-23 10:45:23 -0700

    implement mapreduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,29 +1,133 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"time"
+)
 
 
 type Coordinator struct {
-	// Your definitions here.
-
+	files []string
+	nReduce int
+	assignedFiles map[string]bool
+	doneMap map[string]bool
+	doneReduce map[string]bool
+	mapWorkers map[string]string
+	mapWorkersTime map[string]int64
+	reduceWorkers map[string]string
+	reduceWorkersTime map[string]int64
 }
 
-// Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) Assignment(args *WorkerArgs, reply *WorkerReply) error {
+	nextUnassignedFileForMap := c.pickNextUnassignedFileForMap()
+	if nextUnassignedFileForMap != "" {
+		fmt.Printf("Assigning file %v to worker %v\n", nextUnassignedFileForMap, args.WorkerId)
+		c.mapWorkers[args.WorkerId] = nextUnassignedFileForMap
+		c.mapWorkersTime[args.WorkerId] = time.Now().Unix()
+		reply.File = nextUnassignedFileForMap
+		reply.WorkType = "map"
+		reply.NReduce = c.nReduce
+		return nil
+	}
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+	for _, file := range c.files {
+		if !c.doneMap[file] {
+			reply.File = ""
+			return nil
+		}
+	}
+
+	nextUnassignedReduce := c.pickNextUnassignedReduce()
+	if nextUnassignedReduce != "" {
+		fmt.Printf("Assigning reduce %v to worker %v\n", nextUnassignedReduce, args.WorkerId)
+		c.reduceWorkers[args.WorkerId] = nextUnassignedReduce
+		c.reduceWorkersTime[args.WorkerId] = time.Now().Unix()
+		reply.File = nextUnassignedReduce
+		reply.WorkType = "reduce"
+		reply.NReduce = c.nReduce
+		return nil
+	}
+	reply.File = ""
 	return nil
 }
 
+func (c *Coordinator) DoneMap(args *WorkerArgs, reply *WorkerReply) error {
+	fmt.Printf("Worker %v has finished processing file %v\n", args.WorkerId, c.mapWorkers[args.WorkerId])
+	c.doneMap[c.mapWorkers[args.WorkerId]] = true
+	return nil
+}
+
+func (c *Coordinator) DoneReduce(args *WorkerArgs, reply *WorkerReply) error {
+	fmt.Printf("Worker %v has finished processing reduce %v\n", args.WorkerId, c.reduceWorkers[args.WorkerId])
+	c.doneReduce[c.reduceWorkers[args.WorkerId]] = true
+	return nil
+}
+
+func (c *Coordinator) pickNextUnassignedFileForMap() string {
+	for _, file := range c.files {
+		if !c.assignedFiles[file] {
+			c.assignedFiles[file] = true
+			return file
+		}
+	}
+	return ""
+}
+
+func (c *Coordinator) pickNextUnassignedReduce() string {
+	for i := 0; i < c.nReduce; i++ {
+		iAsString := fmt.Sprintf("%d", i)
+		if !c.doneReduce[iAsString] {
+			c.doneReduce[iAsString] = true
+			return iAsString
+		}
+	}
+	return ""
+}
+
+//
+// main/mrcoordinator.go calls Done() periodically to find out
+// if the entire job has finished.
+//
+func (c *Coordinator) Done() bool {
+	for workerID, timestamp := range c.mapWorkersTime {
+		if time.Now().Unix()-timestamp > 10 {
+			file := c.mapWorkers[workerID]
+			if !c.doneMap[file] {
+				c.assignedFiles[file] = false
+			}
+			delete(c.mapWorkers, workerID)
+			delete(c.mapWorkersTime, workerID)
+		}
+	}
+
+	for _, file := range c.files {
+		if !c.doneMap[file] {
+			return false
+		}
+	} // all files are done with map phase
+
+	for workerID, timestamp := range c.reduceWorkersTime {
+		if time.Now().Unix()-timestamp > 10 {
+			file := c.reduceWorkers[workerID]
+			c.doneReduce[file] = false
+			delete(c.reduceWorkers, workerID)
+		}
+	}
+
+	for i := 0; i < c.nReduce; i++ {
+		iAsString := fmt.Sprintf("%d", i)
+		if !c.doneReduce[iAsString] {
+			return false
+		}
+	} // all files are done with reduce phase
+
+	return true
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -42,29 +146,34 @@ func (c *Coordinator) server() {
 }
 
 //
-// main/mrcoordinator.go calls Done() periodically to find out
-// if the entire job has finished.
-//
-func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
-}
-
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		files: files,
+		nReduce: nReduce,
+		assignedFiles: make(map[string]bool),
+		doneMap: make(map[string]bool),
+		doneReduce: make(map[string]bool),
+		mapWorkers: make(map[string]string),
+		mapWorkersTime: make(map[string]int64),
+		reduceWorkers: make(map[string]string),
+		reduceWorkersTime: make(map[string]int64),
+	}
+	fmt.Printf("Starting coordinator server\n")
 
-	// Your code here.
+	for _, file := range files {
+		c.assignedFiles[file] = false
+		c.doneMap[file] = false
+	}
 
-
+	for i := 0; i < nReduce; i++ {
+		iAsString := fmt.Sprintf("%d", i)
+		c.doneReduce[iAsString] = false
+	}
+	
 	c.server()
 	return &c
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,20 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
 
+	"github.com/rs/xid"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,6 +23,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+var id = xid.New().String()
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -31,40 +49,170 @@ func ihash(key string) int {
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	for {
+		AskAssignment(mapf, reducef)
+		time.Sleep(5 * time.Second)
+	}
 
 }
 
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
+func AskAssignment(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+	args := WorkerArgs{}
+	args.WorkerId = id
+	reply := WorkerReply{}
+	ok := call("Coordinator.Assignment", &args, &reply)
 	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
+		doAssignment(reply, mapf, reducef)
 	} else {
 		fmt.Printf("call failed!\n")
 	}
+}
+
+func DoneMap() {
+	args := WorkerArgs{}
+	args.WorkerId = id
+	reply := WorkerReply{}
+	ok := call("Coordinator.DoneMap", &args, &reply)
+	if !ok {
+		fmt.Printf("call failed!\n")
+	}
+}
+
+func DoneReduce() {
+	args := WorkerArgs{}
+	args.WorkerId = id
+	reply := WorkerReply{}
+	ok := call("Coordinator.DoneReduce", &args, &reply)
+	if !ok {
+		fmt.Printf("call failed!\n")
+	}
+}
+
+func doAssignment(reply WorkerReply, mapf func( string,  string) []KeyValue, reducef func( string,  []string) string) {
+	fmt.Printf("reply.File %v\n", reply.File)
+	fmt.Printf("reply.WorkType %v\n", reply.WorkType)
+	if reply.File == "" {
+		fmt.Printf("No more files to process\n")
+		return
+	}
+	if reply.WorkType == "map" {
+		doMapTask(reply.File, reply.NReduce, mapf)
+		DoneMap()
+	} else if reply.WorkType == "reduce" {
+		doReduceTask(reply.File, reducef)
+		DoneReduce()
+	}
+}
+
+func doMapTask(filename string, nReduce int, mapf func( string,  string) []KeyValue) {
+	fmt.Printf("doMapTask %v\n", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+	fmt.Printf("Looping through key-values after map\n")
+	
+	encoders := make([]*json.Encoder, nReduce)
+	tempFiles := make([]*os.File, nReduce)
+	for i := 0; i < nReduce; i++ {
+		oname := fmt.Sprintf("mr-%v-%v", i, id)
+		ofile, err := os.CreateTemp("", oname)
+		tempFiles[i] = ofile
+		if err != nil {
+			log.Fatalf("cannot open %v", oname)
+		}
+		encoders[i] = json.NewEncoder(ofile)
+	}
+
+	for _, kv := range kva {
+		reduce := ihash(kv.Key) % nReduce
+		encoders[reduce].Encode(&kv)
+	}
+	for i, tempFile := range tempFiles {
+		tempFile.Close()
+		oname := tempFile.Name()
+		newName := filepath.Join(".", fmt.Sprintf("mr-%v-%v", i, id))
+		if _, err := os.Stat(newName); err == nil {
+			// newName file exists, append the contents of oname into newName
+			nf, err := os.OpenFile(newName, os.O_APPEND|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatalf("cannot open %v: %v", newName, err)
+			}
+			of, err := os.Open(oname)
+			if err != nil {
+				log.Fatalf("cannot open %v: %v", oname, err)
+			}
+			defer nf.Close()
+			defer of.Close()
+			_, err = io.Copy(nf, of)
+			if err != nil {
+				log.Fatalf("cannot append %v to %v: %v", oname, newName, err)
+			}
+		} else {
+			// newName file does not exist, rename oname to newName
+			err := os.Rename(oname, newName)
+			if err != nil {
+				log.Fatalf("cannot move %v to current directory: %v", oname, err)
+			}
+		}
+	}
+	fmt.Printf("Done with map\n")
+}
+
+func doReduceTask(reduceChunk string, reducef func( string,  []string) string) {
+	fmt.Printf("doReduceTask %v\n", reduceChunk)
+
+	intermediate := []KeyValue{}
+
+	files, err := filepath.Glob(fmt.Sprintf("mr-%v-*", reduceChunk))
+	if err != nil {
+		log.Fatalf("cannot find files: %v", err)
+	}
+
+	// Open each file and read each line using json decode
+	for _, file := range files {
+		fmt.Printf("Reading file %v\n", file)
+		f, err := os.Open(file)
+		if err != nil {
+			log.Fatalf("cannot open file %v: %v", file, err)
+		}
+		defer f.Close()
+
+		dec := json.NewDecoder(f)
+		for dec.More() {
+			var kv KeyValue
+			err := dec.Decode(&kv)
+			if err != nil {
+				log.Fatalf("cannot decode file %v: %v", file, err)
+			}
+			intermediate = append(intermediate, kv)
+		}
+	}
+	sort.Sort(ByKey(intermediate))
+	oname := fmt.Sprintf("mr-out-%v", reduceChunk)
+	ofile, _ := os.Create(oname)
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	fmt.Printf("Done with reduce for chunk %v\n", reduceChunk)
 }
 
 //
