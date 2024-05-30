f649543c3b883f5b25fd5ac3bf1ef1033dce4ab9
Akshob Gopalakrishna <info@akshob.in>
2024-05-29 23:24:47 -0700

    kv server with no network failures

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,8 +1,12 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+
+	"6.5840/labrpc"
+)
 
 
 type Clerk struct {
@@ -35,9 +39,16 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
+	args := GetArgs{
+		Key: key,
+	}
+	reply := GetReply{}
+	ok := ck.server.Call("KVServer.Get", &args, &reply)
+	if !ok {
+		fmt.Println("Get failed")
+	}
 
-	// You will have to modify this function.
-	return ""
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -49,8 +60,16 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
-	// You will have to modify this function.
-	return ""
+	args := PutAppendArgs{
+		Key: key,
+		Value: value,
+	}
+	reply := PutAppendReply{}
+	ok := ck.server.Call("KVServer." + op, &args, &reply)
+	if !ok {
+		fmt.Println(op + " failed")
+	}
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
